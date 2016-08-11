fbb849fa699b7db59da5a2bf7670c02a39d0fbe9
“reddrum” <anton.kranz@me.com>
2016-08-12 02:07:28 +0300

    Update migrations

--- app.rb
+++ app.rb
@@ -15,6 +15,6 @@ class Barber < ActiveRecord::Base
 end
 
 get '/' do
-  @barbers = Barber.all
+  @barbers = Barber.order "created_at DESC"
 	erb :index
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160811222145) do
+ActiveRecord::Schema.define(version: 20160811230555) do
 
   create_table "barbers", force: :cascade do |t|
     t.text     "name"
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 20160811222145) do
   end
 
   create_table "client", force: :cascade do |t|
+    t.text     "name"
+    t.text     "phone"
+    t.text     "datestamp"
+    t.text     "barber"
+    t.text     "color"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "clients", force: :cascade do |t|
     t.text     "name"
     t.text     "phone"
     t.text     "datestamp"
